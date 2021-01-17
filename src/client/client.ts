7263404f6252bcb778c367f0f3d8b454f9dffabf
Maksim Kokuysky <m.kokuysky@corp.101xp.com>
2021-01-17 14:47:43 +0300

    Fix

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,55 +1,134 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import {GUI} from 'three/examples/jsm/libs/dat.gui.module';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 
-const scene: THREE.Scene = new THREE.Scene();
+const scene: THREE.Scene = new THREE.Scene()
+//scene.background = new THREE.Color(0xff0000)
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
 
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const light = new THREE.PointLight(0xffffff, 2);
+light.position.set(10, 10, 10);
+scene.add(light);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry();
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#00ff00', wireframe: true });
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const controls = new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change', render)
 
-camera.position.z = 2;
+const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry()
+const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry()
+const icosahedronGeometry: THREE.IcosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
+const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry()
+const torusKnotGeometry: THREE.TorusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const stats = Stats();
-document.body.appendChild(stats.dom);
+const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial()
 
-const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01);
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01);
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01);
-cubeFolder.open();
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, 'z', 0, 10, 0.1);
+const texture = new THREE.TextureLoader().load("img/grid.png")
+material.map = texture
+const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
+envTexture.mapping = THREE.CubeReflectionMapping
+//envTexture.mapping = THREE.CubeRefractionMapping
+material.envMap = envTexture
 
-window.addEventListener('resize', onWindowResize, false);
+const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, material)
+cube.position.x = 5
+scene.add(cube)
 
+const sphere: THREE.Mesh = new THREE.Mesh(sphereGeometry, material)
+sphere.position.x = 3
+scene.add(sphere)
+
+const icosahedron: THREE.Mesh = new THREE.Mesh(icosahedronGeometry, material)
+icosahedron.position.x = 0
+scene.add(icosahedron)
+
+const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
+plane.position.x = -2
+scene.add(plane)
+
+const torusKnot: THREE.Mesh = new THREE.Mesh(torusKnotGeometry, material)
+torusKnot.position.x = -5
+scene.add(torusKnot)
+
+camera.position.z = 3
+
+window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	render()
 }
 
-function animate () {
-	requestAnimationFrame(animate);
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-	controls.update();
+var options = {
+	side: {
+		"FrontSide": THREE.FrontSide,
+		"BackSide": THREE.BackSide,
+		"DoubleSide": THREE.DoubleSide,
+	},
+	combine: {
+		"MultiplyOperation": THREE.MultiplyOperation,
+		"MixOperation": THREE.MixOperation,
+		"AddOperation": THREE.AddOperation
+	},
+}
+const gui = new GUI()
 
-	renderer.render(scene, camera);
+const materialFolder = gui.addFolder('THREE.Material')
+materialFolder.add(material, 'transparent')
+materialFolder.add(material, 'opacity', 0, 1, 0.01)
+materialFolder.add(material, 'depthTest')
+materialFolder.add(material, 'depthWrite')
+materialFolder.add(material, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
+materialFolder.add(material, 'visible')
+materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
+materialFolder.open()
 
-	stats.update();
+var data = {
+	color: material.color.getHex(),
+	emissive: material.emissive.getHex(),
+	specular: material.specular.getHex()
+};
+
+var meshPhongMaterialFolder = gui.addFolder('THREE.MeshPhongMaterial');
+
+meshPhongMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) });
+meshPhongMaterialFolder.addColor(data, 'emissive').onChange(() => { material.emissive.setHex(Number(data.emissive.toString().replace('#', '0x'))) });
+meshPhongMaterialFolder.addColor(data, 'specular').onChange(() => { material.specular.setHex(Number(data.specular.toString().replace('#', '0x'))) });
+meshPhongMaterialFolder.add(material, 'shininess', 0, 1024);
+meshPhongMaterialFolder.add(material, 'wireframe');
+meshPhongMaterialFolder.add(material, 'wireframeLinewidth', 0, 10);
+meshPhongMaterialFolder.add(material, 'flatShading').onChange(() => updateMaterial())
+meshPhongMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial())
+meshPhongMaterialFolder.add(material, 'reflectivity', 0, 1);
+meshPhongMaterialFolder.add(material, 'refractionRatio', 0, 1);
+meshPhongMaterialFolder.open()
+
+function updateMaterial() {
+	material.side = Number(material.side)
+	material.combine = Number(material.combine)
+	material.needsUpdate = true
 }
 
+var animate = function () {
+	requestAnimationFrame(animate)
+
+	render()
+
+	stats.update()
+};
+
+function render() {
+	renderer.render(scene, camera)
+}
 animate();
